d5e3d332be5a7319a0ff10704362ca0b00848560
Komi Rodrigue KOUDAKPO <gabirusamaa@gmail.com>
2025-10-25 16:49:53 +0000

    feat: Configure app branding and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:s_contact/app.dart';
 import 'package:s_contact/data/local_storage.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   // Initialiser le stockage local
   await LocalStorage.init();
+
+  // Remove splash screen after initialization is complete
+  FlutterNativeSplash.remove();
 
   runApp(const ProviderScope(child: App()));
 }
